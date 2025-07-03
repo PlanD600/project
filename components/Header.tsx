3083c00382117d1562e2a57f022d7fea3141db08
PlanD600 <pland@mypland.com>
2025-07-03 11:47:46 +0300

    fix: Correct destructuring of user from useAuthStore in Header component

--- components/Header.tsx
+++ components/Header.tsx
@@ -14,7 +14,7 @@ interface HeaderProps {
 }
 
 const Header: React.FC<HeaderProps> = ({ onGoToSettings, projectsForCurrentUser }) => {
-  const { currentUser, handleLogout } = useAuthStore();
+  const { user: currentUser, handleLogout } = useAuthStore();
   const {
     notifications,
     handleSetNotificationsRead,
